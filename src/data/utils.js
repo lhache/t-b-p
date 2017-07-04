ac90ec6bce5f09bd1ed1a56595c872cc06b62afe
lhache <hache.louis@gmail.com>
2017-07-04 19:58:19 +0200

    create utils file

---
+++ src/data/utils.js
@@ -0,0 +1,11 @@
+
+export const parseQueryString = (str = "") => {
+  let objURL = {};
+  str.replace(
+      new RegExp( "([^?=&]+)(=([^&]*))?", "g" ),
+      function( $0, $1, $2, $3 ){
+          objURL[ $1 ] = $3;
+      }
+  );
+  return objURL;
+};
