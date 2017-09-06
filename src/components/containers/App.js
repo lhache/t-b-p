1ccbfdb0790e272a4fafdbc31f4a38ffde4a995e
lhache <hache.louis@gmail.com>
2017-09-06 12:47:46 +0200

    comment static pages in routes

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -6,10 +6,10 @@ import ResultsPage from '../pages/ResultsPage'
 import DetailsPage from '../pages/DetailsPage'
 import LandingPage from '../pages/LandingPage'
 import ContactPage from '../pages/static/ContactPage'
-import AboutUsPage from '../pages/static/AboutUsPage'
-import ImprintPage from '../pages/static/ImprintPage'
-import PrivacyPolicyPage from '../pages/static/PrivacyPolicyPage'
-import AffiliateDisclaimerPage from '../pages/static/AffiliateDisclaimerPage'
+// import AboutUsPage from '../pages/static/AboutUsPage'
+// import ImprintPage from '../pages/static/ImprintPage'
+// import PrivacyPolicyPage from '../pages/static/PrivacyPolicyPage'
+// import AffiliateDisclaimerPage from '../pages/static/AffiliateDisclaimerPage'
 import Flexbox from 'flexbox-react';
 import { isDeviceConsideredMobile } from '../../data/utils'
 import './App.css';
@@ -29,10 +29,10 @@ class App extends Component {
 
         {/* static pages */}
         <Route path="/static/contact" component={ContactPage} />
-        <Route path="/static/about" component={AboutUsPage} />
+        {/* <Route path="/static/about" component={AboutUsPage} />
         <Route path="/static/imprint" component={ImprintPage} />
         <Route path="/static/privacy-policy" component={PrivacyPolicyPage} />
-        <Route path="/static/affiliate-disclaimer" component={AffiliateDisclaimerPage} />
+        <Route path="/static/affiliate-disclaimer" component={AffiliateDisclaimerPage} /> */}
       </Flexbox>
     );
   }
