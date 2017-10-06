13e16fce663ecd171716ddc3f4774accf1e8d5d6
lhache <hache.louis@gmail.com>
2017-10-06 12:47:07 +0200

    bump react to 16 and add portal on cookie banner bar

--- src/components/presentational/CookieBannerBar.js
+++ src/components/presentational/CookieBannerBar.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import CookieBanner from 'react-cookie-banner';
 import Flexbox from 'flexbox-react';
 import counterpart from 'counterpart'
 // import './CookieBannerBar.css'
 
-const CookieBannerBar = () => {
-  return (
-    <Flexbox className="CookieBanner" flexBasis="100%">
-      <CookieBanner
-        message={counterpart('cookies.bannerText')}
-        onAccept={() => {}}
-        cookie='user-has-accepted-cookies'
-        dismissOnScroll={false}
-      />
-    </Flexbox>
-  )
+class CookieBannerBar extends Component {
+  constructor(props) {
+    super(props)
+
+    let wrapper = document.getElementById('cookie-banner-wrapper')
+
+    // create the element if it's not there
+    // mainly a temp hack for development setup
+    if (!this.element) {
+      wrapper = document.createElement('div')
+      wrapper.setAttribute('id', 'cookie-banner-wrapper')
+    }
+
+    this.portalElement = wrapper;
+
+  }
+
+  render() {
+    return ReactDOM.createPortal(
+      <Flexbox className="CookieBanner" flexBasis="100%">
+        <CookieBanner
+          message={counterpart('cookies.bannerText')}
+          onAccept={() => {}}
+          cookie='user-has-accepted-cookies'
+          dismissOnScroll={false}
+        />
+      </Flexbox>,
+      this.portalElement,
+    );
+  }
 }
+
 
 export default CookieBannerBar;
